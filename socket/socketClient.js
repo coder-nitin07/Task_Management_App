40e4e4fb317d953af33d86b22c8499834f1d352f
coder-nitin07 <nitinthakur87504@gmail.com>
2025-04-21 15:47:29 +0530

    Update socketClient.js

--- socket/socketClient.js
+++ socket/socketClient.js
@@ -1,14 +1,12 @@
 const io = require('socket.io-client');
 
-// Replace with your backend URL
-const socket = io('http://localhost:3000'); // assuming your backend is running on port 5000
+const socket = io('http://localhost:3000');
 
 // Connect to the server
 socket.on('connect', () => {
     console.log('Connected to the Socket.IO server!');
 });
 
-// Listen for the "taskCreated" event
 socket.on('taskCreated', (task) => {
     console.log('New Task Created:', task);
 });
