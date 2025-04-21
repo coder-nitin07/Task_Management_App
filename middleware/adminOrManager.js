6cb00d6830bfa7a76c928f6763baaed451810907
coder-nitin07 <nitinthakur87504@gmail.com>
2025-04-21 15:51:34 +0530

    Update adminOrManager.js

--- middleware/adminOrManager.js
+++ middleware/adminOrManager.js
@@ -1,18 +1,17 @@
-// middleware/roleMiddleware.js
-
 const adminOrManager = (allowedRoles) => {
     return (req, res, next) => {
-      // Ensure user is authenticated
+      
+      // user should be authenticated
       if (!req.user) {
         return res.status(401).json({ message: "Unauthorized: User not logged in" });
       }
   
-      // Check if user's role is allowed
+      // Checking the user's role is allowed
       if (!allowedRoles.includes(req.user.role)) {
         return res.status(403).json({ message: "Forbidden: Access denied" });
       }
   
-      next(); // User is allowed, move on
+      next();
     };
   };
   
